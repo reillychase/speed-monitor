cd096b799935facbfc19d9b59e513a7ec1954a28
Reilly Chase <rchase@rchase.com>
2016-05-05 16:24:32 -0500

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// DB Config of first chart
+$nameofdb = '';
+$dbusername = '';
+$dbpassword = '';
+$host = 'localhost';
+
+// DB config of second chart
+$nameofdb2 = '';
+$dbusername2 = '';
+$dbpassword2 = '';
+$host2 = 'localhost';
+
+
+
+?>
