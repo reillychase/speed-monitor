8181e0a774f10330051e08e99de6b59a53ff2627
Reilly Chase <rchase@rchase.com>
2016-05-06 16:10:20 -0500

    Updated MySQL skeleton

--- MySQL_db_skeleton.sql
+++ MySQL_db_skeleton.sql
@@ -3,6 +3,10 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `comcast_uptime`;
+CREATE DATABASE `comcast_uptime` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `comcast_uptime`;
+
 DROP TABLE IF EXISTS `residential`;
 CREATE TABLE `residential` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
